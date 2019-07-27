268bbf0d72f27753c113279f02f8956423b48c7e
Antonio Santos <aj.santos128@gmail.com>
2019-07-26 20:48:47 -0400

    Extend UITextField to add Done button for Decimal Keypad.

---
+++ Expenses/Extensions/UITextField.swift
@@ -0,0 +1,29 @@
+//
+//  UITextField.swift
+//  expenses
+//
+//  Created by Antonio Santos on 7/26/19.
+//  Copyright © 2019 Antonio Santos. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    func addDoneButtonToKeyboard(myAction:Selector?){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 300, height: 40))
+        doneToolbar.barStyle = UIBarStyle.default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: myAction)
+        
+        var items = [UIBarButtonItem]()
+        items.append(flexSpace)
+        items.append(done)
+        
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+}
