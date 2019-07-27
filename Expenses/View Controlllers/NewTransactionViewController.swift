780445457b8d0fc03bfda064c341950c5bc0cadb
Antonio Santos <aj.santos128@gmail.com>
2019-07-26 20:50:12 -0400

    Implement delegate for TextFields. Adds features such as closing keyboard and transferring control from one view to another.

--- Expenses/View Controlllers/NewTransactionViewController.swift
+++ Expenses/View Controlllers/NewTransactionViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class NewTransactionViewController: UIViewController {
+class NewTransactionViewController: UIViewController, UITextFieldDelegate {
+    /*
+     This value is either passed by `TransactionTableViewController` in `prepare(for:sender:)`
+     or constructed as part of adding a new transaction.
+     */
+    var entry: Entry?
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var transactionDescriptionLabel: UILabel!
+    @IBOutlet var transactionDescriptionTextField: UITextField!
+    @IBOutlet var transactionAmountLabel: UILabel!
+    @IBOutlet var transactionAmountTextField: UITextField!
+    
+    let TRANSACTION_DESCRIPTION_TEXT_FIELD_TAG = 0
+    let TRANSACTION_AMOUNT_TEXT_FIELD_TAG = 1
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("NewTransactionViewController loaded")
-
-        // Do any additional setup after loading the view.
+        
+        // Handle textfield input through delegate callbacks.
+        transactionDescriptionTextField.delegate = self
+        transactionAmountTextField.delegate = self
+        
+        // Adds done button to keypad
+        transactionAmountTextField.addDoneButtonToKeyboard(myAction: #selector(self.transactionAmountTextField.resignFirstResponder))
+    }
+    
+    // MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // If the First Responder is the Description Text Field, transfer First Responder to Amount Text Field
+        if (textField.tag == TRANSACTION_DESCRIPTION_TEXT_FIELD_TAG) {
+            transactionAmountTextField.becomeFirstResponder()
+        }
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: - IBActions
+    @IBAction func cancelButtonTapped(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func saveButtonTapped(sender: UIBarButtonItem) {
     }
     
 
