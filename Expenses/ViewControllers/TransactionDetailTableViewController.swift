bd30af7e86df99a44014d791ec1d6b53b1ea5da6
Antonio Santos <aj.santos128@gmail.com>
2019-08-14 22:23:26 -0400

    Changes Payee so that it is input in the same view instead of transitioning to a new view.

--- Expenses/ViewControllers/TransactionDetailTableViewController.swift
+++ Expenses/ViewControllers/TransactionDetailTableViewController.swift
@@ -19,7 +19,7 @@ class TransactionDetailTableViewController: UITableViewController, UITextFieldDe
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBOutlet weak var transactionTypeSwitch: UISwitch!
     @IBOutlet weak var transactionAmountTextField: UITextField!
-    @IBOutlet weak var payeeLabel: UILabel!
+    @IBOutlet weak var payeeTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,129 +32,19 @@ class TransactionDetailTableViewController: UITableViewController, UITextFieldDe
         
         self.transactionAmountTextField.delegate = self
         transactionAmountTextField.text = "$0.00"
+        transactionAmountTextField.addDoneButtonToKeyboard(myAction: #selector(self.transactionAmountTextField.resignFirstResponder))
         
         updateTextFieldColor(isOn: transactionTypeSwitch.isOn)
         
         transactionAmountTextField.becomeFirstResponder()
+        
+        self.payeeTextField.delegate = self
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-    */
-
-    /*
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-    */
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        
-        switch segue.identifier! {
-        case "SelectPayee":
-            os_log("Selecting payee.", log: OSLog.default, type: .debug)
-            
-            guard let destination = segue.destination as? UINavigationController else {
-                fatalError("Unexpected Destination: \(segue.destination)")
-            }
-            
-            guard let targetDestination = destination.topViewController as? PayeesTableViewController else {
-                fatalError("Unexpected Destination: \(segue.destination)")
-            }
-            
-            targetDestination.container = container
-            
-            if let _ = payee {
-                targetDestination.payee = payee as? Payee
-            }
-        default:
-            print("Unknown segue: \(segue.identifier!)")
-        }
-    }
-    
-    @IBAction func unwindToTransactionDetail(sender: UIStoryboardSegue) {
-        let payeeView = sender.source as? PayeesTableViewController
-        if sender.source is PayeesTableViewController {
-            // Receive the transaction back from the TransactionDetailViewController, save it to the persistent storage and
-            // update Persistent Storage.
-            payee = payeeView?.payee
-            do {
-                try container.viewContext.save()
-//                updatePersistentStorage()
-            } catch let error as NSError {
-                print("Could not save. \(error), \(error.userInfo)")
-            }
-            
-            payeeLabel.text = payee.value(forKey: "name") as? String
-
-//            // reset the tableView to defaults if no data message was displayed before loading data.
-//            if self.tableView.backgroundView != nil {
-//                self.tableView.backgroundView = nil
-//                self.tableView.separatorStyle = .singleLine
-//            }
-//            self.tableView.reloadData()
-        }
     }
 
     // MARK: - IBActions
@@ -177,21 +67,27 @@ class TransactionDetailTableViewController: UITableViewController, UITextFieldDe
     
     // MARK: - UITextFieldDelegate
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let char = string.cString(using: String.Encoding.utf8)
-        let isBackSpace = strcmp(char, "\\b") == -92
-        
-        if let text = textField.text {
-            if (isBackSpace) {
-                textField.text = try? Helper.convertStringNumeralToCurrency(from: String(text.dropLast()), add: "")
-                return false
-            } else {
-                let digitCount = Helper.removePunctuation(textField.text!).count + string.count
-                
-                if (digitCount <= 11) {
-                    textField.text = try? Helper.convertStringNumeralToCurrency(from: text, add: string)
+        if (textField.tag == 0) {
+            let char = string.cString(using: String.Encoding.utf8)
+            let isBackSpace = strcmp(char, "\\b") == -92
+            
+            if let text = textField.text {
+                if (isBackSpace) {
+                    textField.text = try? Helper.convertStringNumeralToCurrency(from: String(text.dropLast()), add: "")
                     return false
-                } else { return false }
-            }
+                } else {
+                    let digitCount = Helper.removePunctuation(textField.text!).count + string.count
+                    
+                    if (digitCount <= 11) {
+                        textField.text = try? Helper.convertStringNumeralToCurrency(from: text, add: string)
+                        return false
+                    } else { return false }
+                }
+            } else { return true }
         } else { return true }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
     }
 }
